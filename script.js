679ca157fe7d12eee92cc97b330cdc49a4474db4
phanjosdev <phanjosdev@gmail.com>
2026-03-12 12:02:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirDoces() {
+    document.getElementById("popupDoces").style.display = "flex";
+}
+
+function fecharDoces() {
+    document.getElementById("popupDoces").style.display = "none";
+}
+
+function abrirQueijos() {
+    document.getElementById("popupQueijos").style.display = "flex";
+}
+
+function fecharQueijos() {
+    document.getElementById("popupQueijos").style.display = "none";
+}
